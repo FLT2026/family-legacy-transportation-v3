4a0942f4ea14e3ec5aeca4610f42b32923005832
FLT2026 <after50@yahoo.com>
2026-08-31 17:55:30 -0400

    Refresh next-action navigation highlighting

--- fleet-module.js
+++ fleet-module.js
@@ -8,5 +8,5 @@
     script.addEventListener('error',()=>console.error('Unable to load V3.5 module:',src),{once:true});
     document.body.appendChild(script);
   };
-  loadScript('fleet-core.js?v=20260831f',()=>loadScript('v35-proposed-load.js?v=20260831f',()=>loadScript('v35-guided-workflow.js?v=20260831f')));
+  loadScript('fleet-core.js?v=20260831g',()=>loadScript('v35-proposed-load.js?v=20260831g',()=>loadScript('v35-guided-workflow.js?v=20260831g')));
 })();
