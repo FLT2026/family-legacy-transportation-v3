3dfbd6db65e7538e64ae64a95a24a0abf77f9416
FLT2026 <after50@yahoo.com>
2026-08-31 17:05:36 -0400

    Cache-bust corrected V3.5 fleet modules

--- fleet-module.js
+++ fleet-module.js
@@ -8,5 +8,5 @@
     script.addEventListener('error',()=>console.error('Unable to load V3.5 module:',src),{once:true});
     document.body.appendChild(script);
   };
-  loadScript('fleet-core.js',()=>loadScript('v35-proposed-load.js'));
+  loadScript('fleet-core.js?v=20260831d',()=>loadScript('v35-proposed-load.js?v=20260831d'));
 })();
