e271c27959c0ef5b303223a880f382008b6d6946
FLT2026 <after50@yahoo.com>
2026-09-01 12:24:49 -0400

    Refresh guided workflow sequencing fix

--- fleet-module.js
+++ fleet-module.js
@@ -8,5 +8,5 @@
     script.addEventListener('error',()=>console.error('Unable to load V3.5 module:',src),{once:true});
     document.body.appendChild(script);
   };
-  loadScript('fleet-core.js?v=20260901i',()=>loadScript('v35-proposed-load.js?v=20260901i',()=>loadScript('v35-guided-workflow.js?v=20260901i')));
+  loadScript('fleet-core.js?v=20260901j',()=>loadScript('v35-proposed-load.js?v=20260901j',()=>loadScript('v35-guided-workflow.js?v=20260901j')));
 })();
