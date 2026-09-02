b780faa96c3d40776a2098fd92ba182a3f9550f9
FLT2026 <after50@yahoo.com>
2026-09-01 22:07:24 -0400

    Refresh V3.5 compliance workflow assets

--- fleet-module.js
+++ fleet-module.js
@@ -8,5 +8,5 @@
     script.addEventListener('error',()=>console.error('Unable to load V3.5 module:',src),{once:true});
     document.body.appendChild(script);
   };
-  loadScript('fleet-core.js?v=20260901x',()=>loadScript('v35-guided-workflow.js?v=20260901x',()=>loadScript('v35-fleet-master.js?v=20260901x',()=>loadScript('v35-proposed-load.js?v=20260901x',()=>loadScript('v35-data-controls.js?v=20260901x')))));
+  loadScript('fleet-core.js?v=20260901y',()=>loadScript('v35-guided-workflow.js?v=20260901y',()=>loadScript('v35-fleet-master.js?v=20260901y',()=>loadScript('v35-proposed-load.js?v=20260901y',()=>loadScript('v35-data-controls.js?v=20260901y')))));
 })();
