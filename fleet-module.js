2a4da62d645f4bc1cda2a5601dc9010c4bd14639
FLT2026 <after50@yahoo.com>
2026-09-08 12:33:41 -0400

    Load hard navigation state controller

--- fleet-module.js
+++ fleet-module.js
@@ -20,7 +20,8 @@
     'v38-weight-equipment-ui.js?v=20260908b',
     'v38-document-compliance-ui.js?v=20260908b',
     'v38-pickup-delivery-ui.js?v=20260908b',
-    'v38-fast-load-workflow.js?v=20260908a'
+    'v38-fast-load-workflow.js?v=20260908a',
+    'v38-nav-next-hardfix.js?v=20260908a'
   ];
 
   // HARD DEV CACHE RULE:
