28eaadf39006788dd48c6347023a61c11773f877
FLT2026 <after50@yahoo.com>
2026-09-08 12:44:32 -0400

    Load hard process training controller

--- fleet-module.js
+++ fleet-module.js
@@ -21,7 +21,8 @@
     'v38-document-compliance-ui.js?v=20260908b',
     'v38-pickup-delivery-ui.js?v=20260908b',
     'v38-fast-load-workflow.js?v=20260908a',
-    'v38-nav-next-hardfix.js?v=20260908a'
+    'v38-nav-next-hardfix.js?v=20260908a',
+    'v38-process-training-hardfix.js?v=20260908a'
   ];
 
   // HARD DEV CACHE RULE:
