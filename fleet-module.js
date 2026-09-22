25b7762d4479d29abb282bab167c6b66cc3dbecf
FLT2026 <after50@yahoo.com>
2026-09-22 14:56:41 -0400

    Refresh current rig hardfix asset

--- fleet-module.js
+++ fleet-module.js
@@ -32,7 +32,7 @@
     'v38-v36-autopopulate-hardfix.js?v=20260908a',
     'v38-navigation-prerequisites-hardfix.js?v=20260909a',
     'v38-test-data-reset.js?v=20260909a',
-    'v38-current-rig-display-hardfix.js?v=20260912a',
+    'v38-current-rig-display-hardfix.js?v=20260922b',
     'v38-workflow-authority.js?v=20260910b',
     'v38-fleet-renewal-guidance.js?v=20260912a',
     'v38-master-record-hub.js?v=20260913a',
